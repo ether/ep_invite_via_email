797c70d1a17d6bc2d8e60c083c8c28679920cb76
John McLear <john@mclear.co.uk>
2020-08-22 18:10:24 +0000

    fix it

--- server_invite_via_email.js
+++ server_invite_via_email.js
@@ -5,18 +5,15 @@ exports.expressServer = function (hook_name, args, cb) {
     var name        = req.param('name', null);  // the name of the recipient
     var emailAddy   = req.param('email', null);  // the email address of the recipient
     var padurl      = req.param('padurl', null);  // the url of the pad the recipient is being invited to
-  
-    // Get email and buffer tools dependencies 
-    var email       = require("emailjs/email");
-    var buffertools = require("buffertools");
 
-    // Define the server connection
-    var server  = email.server.connect({
+    var email       = require("emailjs");
+    var SMTPClient  = email.SMTPClient;
+    const server = new SMTPClient({
        host:    "localhost",
        port:    "25",
        ssl:     false
     });
-  
+
     // Send the message and get a callback with an error or details of the message that was sent
     server.send({
        text:    "You have been invited to collaboratively edit the pad at: " +padurl,
