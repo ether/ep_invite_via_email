7413ea3c05292bcfc583e8f1af0049098a41d52a
John McLear <john@mclear.co.uk>
2020-11-22 16:39:20 +0000

    autofixes from Etherpad checkPlugins.js

--- server_invite_via_email.js
+++ server_invite_via_email.js
@@ -1,28 +1,28 @@
 exports.expressServer = function (hook_name, args, cb) {
-  args.app.get('/server_invite_via_email', function(req, res) {
+  args.app.get('/server_invite_via_email', (req, res) => {
     console.log(req);
     // Get the parameters from the POST request
-    var name        = req.param('name', null);  // the name of the recipient
-    var emailAddy   = req.param('email', null);  // the email address of the recipient
-    var padurl      = req.param('padurl', null);  // the url of the pad the recipient is being invited to
+    const name = req.param('name', null); // the name of the recipient
+    const emailAddy = req.param('email', null); // the email address of the recipient
+    const padurl = req.param('padurl', null); // the url of the pad the recipient is being invited to
 
-    var email       = require("emailjs");
-    var SMTPClient  = email.SMTPClient;
+    const email = require('emailjs');
+    const SMTPClient = email.SMTPClient;
     const server = new SMTPClient({
-       host:    "localhost",
-       port:    "25",
-       ssl:     false
+      host: 'localhost',
+      port: '25',
+      ssl: false,
     });
 
     // Send the message and get a callback with an error or details of the message that was sent
     server.send({
-       text:    "You have been invited to collaboratively edit the pad at: " +padurl,
-       from:    "Etherpad <email-invite@etherpad.org>",
-       to:      "<"+emailAddy+">",
-       subject: "You have been invited to a pad"
-    }, function(err, message){
+      text: `You have been invited to collaboratively edit the pad at: ${padurl}`,
+      from: 'Etherpad <email-invite@etherpad.org>',
+      to: `<${emailAddy}>`,
+      subject: 'You have been invited to a pad',
+    }, (err, message) => {
       // console.log(err || message);
       res.send(err || message); // Send the response back to the client
     });
   });
-}
+};
